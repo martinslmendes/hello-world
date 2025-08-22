d08a474f60a1f65be9f12eb882870ed008b29c4c
Lucas Mendes Martins <martinslmendes@gmail.com>
2025-08-21 22:10:18 -0300

    Created first pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
+import NotFound from "./pages/errors/NotFound";
 
 function App() {
     return (
-        <h1>Hello World</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about-me" element={<AboutMe/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
